8c006a1743bc0364714e86a449a55b6feace96e4
Roger Kiew <yuichiu416@gmail.com>
2020-02-21 14:17:42 -0600

    created first burger component w/ ingredients

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+export default class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <Burger />
+                <div>Build Controls</div>
+            </Aux>
+        )
+    }
+}
