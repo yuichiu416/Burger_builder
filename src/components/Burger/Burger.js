39d771d6374df1b593f953e44a32e02446d615b0
Roger Kiew <yuichiu416@gmail.com>
2020-02-21 16:27:03 -0600

    added buildcontrols

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,22 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import './Burger.css';
 
 const burger = props => {
+    let transformedIngredients = Object.keys(props.ingredients)
+      .map((key, idx) => {
+        return [...Array(props.ingredients[key])].map((k, i) => {
+          // Array length is how many of each ingredient we have
+          return <BurgerIngredient key={key + idx + i} type={key} />; // actually render the ingredient
+        });
+      }).reduce((arr, el) => arr.concat(el), []);
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients</p>;
+    }
     return (
-        <div className="Burger">
-            <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="bread-bottom"/>
-        </div>
+      <div className="Burger">
+        <BurgerIngredient type="bread-top" />
+        {transformedIngredients}
+        <BurgerIngredient type="bread-bottom" />
+      </div>
     );
 };
 
