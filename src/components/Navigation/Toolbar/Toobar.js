9381211249b573468f0fa8bfbf6b8483526db4ff
Roger Kiew <yuichiu416@gmail.com>
2020-02-23 01:33:54 -0600

    added sideDrawer, hamburger menu

--- src/components/Navigation/Toolbar/Toobar.js
+++ src/components/Navigation/Toolbar/Toobar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import './Toobar.css';
 
 const toolbar = props => (
     <header className="Toolbar">
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <div className="ToolbarLogo">
             <Logo height="80%"/>
         </div>
-        
-        <NavigationItems/>
+        <nav className="DesktopOnly">
+            <NavigationItems/>
+        </nav>
     </header>
 );
 
