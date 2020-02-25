b58e9f3a0e4f5add3739fbd9bd6d643a6dd66b0d
Roger Kiew <yuichiu416@gmail.com>
2020-02-24 22:00:05 -0600

    reused Modal for error messages and created a withErrorHandler hoc

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,8 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import Spinner from '../../components/UI/Spinner/Spinner';
-import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios-orders';
 
 
 const INGREDIENT_PRICES = {
@@ -15,7 +16,7 @@ const INGREDIENT_PRICES = {
 	bacon: 0.7
 };
 
-export default class BurgerBuilder extends Component {
+class BurgerBuilder extends Component {
 	state = {
 		ingredients: {
 			salad: 0,
@@ -137,3 +138,5 @@ export default class BurgerBuilder extends Component {
     );
 	}
 }
+
+export default withErrorHandler(BurgerBuilder, axios);
