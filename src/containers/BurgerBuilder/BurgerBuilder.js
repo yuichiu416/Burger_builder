d488987eaf56c9d2c2cc07173644de5f1d131d02
Roger Kiew <yuichiu416@gmail.com>
2020-03-03 13:22:58 -0600

    change price to global state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,17 +10,9 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import * as actionTypes from '../../store/actions';
 
-const INGREDIENT_PRICES = {
-	salad: 0.5,
-	cheese: 0.4,
-	meat: 1.3,
-	bacon: 0.7
-};
-
 class BurgerBuilder extends Component {
 	state = {
 		totalPrice: 4,
-		purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -41,7 +33,7 @@ class BurgerBuilder extends Component {
         this.setState( { loading: true } );
         const order = {
             ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
+            price: this.props.price,
             customer: {
                 name: 'Max Schwarzmüller',
                 address: {
@@ -64,7 +56,7 @@ class BurgerBuilder extends Component {
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        queryParams.push('price=' + this.state.totalPrice);
+        queryParams.push('price=' + this.props.price);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
@@ -78,43 +70,8 @@ class BurgerBuilder extends Component {
 		const sum = Object.keys(ingredients)
 			.map(key => ingredients[key])
 			.reduce((sum, el) => sum + el, 0);
-	    this.setState({ purchasable: sum > 0 });
+        return sum > 0;
 	}
-	addIngredientHandler = type => {
-		const oldCount = this.state.ingredients[type];
-		const updatedCount = oldCount + 1;
-		const updatedIngredients = {
-			...this.state.ingredients
-		};
-		updatedIngredients[type] = updatedCount;
-		const priceAddition = INGREDIENT_PRICES[type];
-		const oldPrice = this.state.totalPrice;
-		const newPrice = oldPrice + priceAddition;
-		this.setState({
-			totalPrice: newPrice,
-			ingredients: updatedIngredients
-		});
-		this.updatePurchaseState(updatedIngredients);
-	}
-	removeIngredientHandler = type => {
-		const oldCount = this.state.ingredients[type];
-		if (oldCount <= 0) {
-			return;
-		}
-		const updatedCount = oldCount - 1;
-		const updatedIngredients = {
-			...this.state.ingredients
-		};
-		updatedIngredients[type] = updatedCount;
-		const priceDeduction = INGREDIENT_PRICES[type];
-		const oldPrice = this.state.totalPrice;
-		const newPrice = oldPrice - priceDeduction;
-		this.setState({
-			totalPrice: newPrice,
-			ingredients: updatedIngredients
-		});
-		this.updatePurchaseState(updatedIngredients);
-    }
     purchaseCancelHandler = () => {
         this.setState({ purchasing: false});
     }
@@ -135,8 +92,8 @@ class BurgerBuilder extends Component {
                             ingredientAdded={this.props.onIngredientAdded}
                             ingredientRemoved={this.props.onIngredientRemoved}
                             disabled={disabledInfo}
-                            price={this.state.totalPrice}
-                            purchasable={this.state.purchasable}
+                            price={this.props.price}
+                            purchasable={this.updatePurchaseState(this.props.ings)}
                             ordered={this.purchaseHandler}
                         />
                 </Aux>
@@ -146,7 +103,7 @@ class BurgerBuilder extends Component {
                     ingredients={this.props.ings}
                     purchaseCanceled={this.purchaseCancelHandler}
                     purchaseContinues={this.purchaseContinueHandler}
-                    price={this.state.totalPrice}
+                    price={this.props.price}
                 />
             );
         }
@@ -169,7 +126,8 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.ingredients,
+        price: state.totalPrice
     };
 };
 
